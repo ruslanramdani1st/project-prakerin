821a8b8a91b099a89f8278c9acf5949d2de16715
ruslanramdani1st <ruslanramdani1st@gmail.com>
2022-03-27 14:42:02 +0700

    Membuat API ke semua tabel dan mengganti bahasa

--- resources/views/layouts/transaksi/ticket.blade.php
+++ resources/views/layouts/transaksi/ticket.blade.php
@@ -21,7 +21,7 @@
 <body>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
                         <div class="card-header">
